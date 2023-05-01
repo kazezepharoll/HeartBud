e06286d744edbfe7c704b49cb68a450d982b3bbb
kazeze pharoll <kazezepharoll47@gmail.com>
2023-05-01 23:51:54 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Let's build heartbud app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
